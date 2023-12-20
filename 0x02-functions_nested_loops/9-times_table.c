35a7e08f7ea2734c76a5370c3e5767f3aecb00a2
Aimrane-gib <aimrane80@gmail.com>
2023-12-20 02:28:33 +0100

    tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -28,8 +28,15 @@ void times_table(void)
 			if (j != 9)
 			{
 				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+				if (n < 10 && (i * (j + 1)) < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(' ');
+				}
 			}
 		}
 		_putchar('\n');
