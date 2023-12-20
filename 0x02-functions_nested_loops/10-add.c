a23397ad017663f2cbc1943a842d25480020b7f3
Aimrane-gib <aimrane80@gmail.com>
2023-12-20 02:37:06 +0100

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @a: first number to add
+ * @b: second number to add
+ *
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+	return (n);
+}
