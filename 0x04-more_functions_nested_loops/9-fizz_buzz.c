293c118408064b3125fcc1408e73e9a6320a06a2
Aimrane-gib <aimrane80@gmail.com>
2023-12-21 23:54:01 +0100

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1 ; i <= 100; i++)
+	{
+		if ((i % 5) == 0 && (i % 3) == 0)
+		{
+			printf("FizzBuzz");
+			putchar(' ');
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz");
+			putchar(' ');
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz");
+			putchar(' ');
+		}
+		else
+		{
+			printf("%d", i);
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
